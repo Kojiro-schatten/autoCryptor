c503504c8c9dce0937fc94aef61932361e798af0
KojiroSchatten <banbirou@icloud.com>
2023-08-27 02:23:26 +0900

    feat: buttonを押したら、userが作成される

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -9,6 +9,32 @@ import { VirtualCurrencies } from '@/data/portfolio/VirtualCurrencies';
 Chart.register(...registerables)
 
 export default function Home() {
+  const handleClick = async () => {
+    const url = 'http://localhost:8080/users';
+    const requestBody = {
+        name: "John Doe3"
+    };
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestBody)
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        console.log(data);
+    } catch (error: any) {
+        console.error('There was a problem with the fetch operation:', error.message);
+    }
+};
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -32,6 +58,7 @@ export default function Home() {
       <div>
         <Doughnut data={VirtualCurrencies} />
       </div>
+      <button onClick={handleClick}>button</button>
     </main>
   )
 }
